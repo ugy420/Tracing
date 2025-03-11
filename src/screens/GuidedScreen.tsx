ef1fe2a0b3497bac9d01c6c43761036a554a6bcd
Jimpa <jamtshojimpa@gmail.com>
2025-03-11 09:05:25 +0600

    Add Guided Screeen Page and CardCategory Screen component

--- src/screens/GuidedScreen.tsx
+++ src/screens/GuidedScreen.tsx
@@ -1,24 +1,44 @@
 import {useNavigation, NavigationProp} from '@react-navigation/native';
 import React from 'react';
-import {Button, View, Text, StyleSheet} from 'react-native';
+import {
+  // Image,
+  Button,
+  View,
+  StyleSheet,
+  ImageBackground,
+  Text,
+} from 'react-native';
 import {RootStackParamList} from '../types';
 
 const GuidedScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <Text>Guided Screen</Text>
-      <Button
-        title="Go to UnGuided Screen"
-        onPress={() => navigation.navigate('UnGuided')}
-      />
-      <Button
-        title="Go to Home Screen"
-        onPress={() => navigation.navigate('Home')}
-      />
-    </View>
+    <ImageBackground
+      source={require('../assets/background_images/guided_bg.jpeg')}
+      style={styles.background}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Kuzuzangpo Lhamo!</Text>
+        <View style={styles.headerIcons}>
+          {/* <Image source={require('../assets/icons/star.jpeg')} /> */}
+        </View>
+      </View>
+
+      <View style={styles.container}>
+        <Button
+          title="Go to UnGuided Screen"
+          onPress={() => navigation.navigate('UnGuided')}
+        />
+        <Button
+          title="Go to Home Screen"
+          onPress={() => navigation.navigate('Home')}
+        />
+        <Button
+          title="Go to Guided Category Screen"
+          onPress={() => navigation.navigate('GuidedCategory')}
+        />
+      </View>
+    </ImageBackground>
   );
 };
 
@@ -26,6 +46,33 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  absolute: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+  header: {
+    height: '10%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  headerIcons: {
+    flexDirection: 'row',
     alignItems: 'center',
   },
 });

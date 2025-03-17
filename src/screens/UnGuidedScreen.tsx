fd1a67e4376b92ca79c26b08d8e7d6720cc77d1a
Jimpa <jamtshojimpa@gmail.com>
2025-03-17 21:01:22 +0600

    Add Landing Screen for UnGuided

--- src/screens/UnGuidedScreen.tsx
+++ src/screens/UnGuidedScreen.tsx
@@ -1,20 +1,250 @@
+import {useNavigation, NavigationProp} from '@react-navigation/native';
 import React from 'react';
-import {View, Text, Button} from 'react-native';
-import {NavigationProp, useNavigation} from '@react-navigation/native';
+import {
+  Image,
+  View,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import {RootStackParamList} from '../types';
+
+type GridItem = {
+  image: any;
+  subText: string;
+  background: any;
+  screen: keyof RootStackParamList;
+};
 
 const UnGuidedScreen = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
 
+  const gridItems: GridItem[] = [
+    {
+      image: require('../assets/icons/animalIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/blueBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/familyIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/blueBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/weatherIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/blueBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/shapeIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/brownBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/clothingIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/brownBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/expressionIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/brownBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/sportsIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/brownBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/fruitsIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/brownBox.png'),
+      screen: 'GuidedCategory',
+    },
+    {
+      image: require('../assets/icons/familyIcon.png'),
+      subText: 'Subtext',
+      background: require('../assets/icons/brownBox.png'),
+      screen: 'GuidedCategory',
+    },
+  ];
+
   return (
-    <View>
-      <Text>UnGuided Screen</Text>
-      <Button
-        title="Go to Guided Screen"
-        onPress={() => navigation.navigate('Guided')}
-      />
-    </View>
+    <ImageBackground
+      source={require('../assets/background_images/landing_bg.png')}
+      style={styles.background}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Kuzuzangpo, Lhamo!</Text>
+        <View style={styles.headerIcons}>
+          {/* <View style={styles.starCount}>
+            <View style={styles.outerRectangle}>
+              <View style={styles.innerRectangle}>
+                <Image
+                  source={require('../assets/icons/star.png')}
+                  style={styles.halfstar}
+                />
+                <Text style={styles.scoreText}>500</Text>
+              </View>
+            </View>
+          </View> */}
+
+          <View style={styles.iconContainer}>
+            <Image
+              source={require('../assets/icons/award.png')}
+              style={styles.icon}
+            />
+          </View>
+          <View style={styles.iconContainer}>
+            <Image
+              source={require('../assets/icons/setting.png')}
+              style={styles.icon}
+            />
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.grid}>
+          {gridItems.map((item, index) => (
+            <TouchableOpacity
+              key={index}
+              style={styles.gridItem}
+              onPress={() => navigation.navigate(item.screen)}>
+              <ImageBackground
+                source={item.background}
+                style={styles.gridItemBackground}>
+                <Image source={item.image} style={styles.gridItemImage} />
+                <Text style={styles.gridItemSubText}>{item.subText}</Text>
+              </ImageBackground>
+            </TouchableOpacity>
+          ))}
+        </View>
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.navigate('UnGuided')}>
+          <Image
+            source={require('../assets/icons/back.png')}
+            style={styles.backButtonIcon}
+          />
+        </TouchableOpacity>
+      </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  header: {
+    height: '15%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#D9D9D9',
+    opacity: 0.6,
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 32,
+    fontFamily: 'Knewave-Regular',
+    fontWeight: 'bold',
+  },
+  headerIcons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 25,
+    backgroundColor: '#2682F4',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+  },
+  icon: {
+    width: 30,
+    height: 30,
+    marginHorizontal: 3,
+  },
+  // starCount: {
+  //   position: 'relative',
+  // },
+  // outerRectangle: {
+  //   width: '100%',
+  //   height: 30,
+  //   borderRadius: 20,
+  //   backgroundColor: '#7E96E4',
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  // },
+  // scoreText: {
+  //   color: 'white',
+  //   fontSize: 24,
+  //   fontWeight: 'bold',
+  // },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  grid: {
+    width: '35%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  gridItem: {
+    width: '28%',
+    aspectRatio: 1,
+    margin: '1%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+  },
+  gridItemBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridItemImage: {
+    width: 50,
+    height: 50,
+    resizeMode: 'contain',
+  },
+  gridItemSubText: {
+    fontSize: 16,
+    color: 'white',
+  },
+  backButton: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 20,
+  },
+  backButtonIcon: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
 
 export default UnGuidedScreen;
